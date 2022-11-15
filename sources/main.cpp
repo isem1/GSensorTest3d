e435e386f49e2e3b26fef63b876586bb6ebac36a
e1sem <deformazzia@gmail.com>
2022-11-15 02:30:37 +0200

    Include and start using qtquick implementation instead of qwidget

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,13 +1,20 @@
 #include <QGuiApplication>
-#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QtQuick/QtQuick>
+#include <QQmlContext>
 
 #include "mainwindow.hpp"
 
 int main(int argc, char** argv) {
-    QApplication app(argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    MainWindow widget;
-    widget.show();
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    if (engine.rootObjects().isEmpty())
+        return EXIT_FAILURE;
 
     return app.exec();
 }
