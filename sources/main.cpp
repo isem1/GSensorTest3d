5b9280d8b65607154454d4a0fc460991e80ab942
e1sem <deformazzia@gmail.com>
2022-11-17 19:38:20 +0200

    Register new SerialPortReader class

--- sources/main.cpp
+++ sources/main.cpp
@@ -2,12 +2,15 @@
 #include <QQmlApplicationEngine>
 
 #include "model/PortSettingsModel.h"
+#include "core/SerialPortReader.h"
 
 int main(int argc, char** argv) {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
     PortSettingsModel::registerQmlType();
+    SerialPortReader::registerQmlType();
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
